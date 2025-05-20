051c892746950a4cb7e203421f9b3884c798a57b
shrusti3 <shrustijeon@gmail.com>
2025-05-20 18:17:12 +0530

    Update Testimonials.jsx

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
       <div className="w-full h-full md:px-10 px-5">
         <TitleHeader
           title="What People Say About Me?"
-          sub="⭐️ Customer feedback highlights"
+          sub="⭐️ highlights"
         />
 
         <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
