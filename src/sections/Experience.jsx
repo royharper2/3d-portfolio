ce43d024cf6b291ba0136edff2fd74fe5a960447
shrusti3 <shrustijeon@gmail.com>
2025-05-20 18:11:40 +0530

    Update Experience.jsx

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -96,8 +96,8 @@ const Experience = () => {
     >
       <div className="w-full h-full md:px-20 px-5">
         <TitleHeader
-          title="Professional Work Experience"
-          sub="💼 My Career Overview"
+          title="Professional Experience"
+          sub="💼 Overview"
         />
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
